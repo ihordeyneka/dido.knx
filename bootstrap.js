c2b191c9e663915006d5d3a37ccb749dd5b41696
Ihor Deyneka <ihor@univisia.com>
2021-01-03 18:36:43 +0200

    Updated token URI

--- bootstrap.js
+++ bootstrap.js
@@ -23,7 +23,8 @@ server.use(function (req, res, next) {
   var isUrlSecured = function (url) {
     if (url.startsWith("/.well-known/acme-challenge"))
       return false;
-    if (url.startsWith("/login.html") || url.endsWith(".js") || url.endsWith(".js.map") || url.endsWith(".ico") || url.endsWith(".json"))
+    if (url.startsWith("/login.html") || url.startsWith("/api/token") || 
+      url.endsWith(".js") || url.endsWith(".js.map") || url.endsWith(".ico") || url.endsWith(".json"))
       return false;
     return true;
   }
